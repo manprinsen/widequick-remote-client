752cd7616fa4cfe862dee4356a0f73ba6079cc7d
Tobias Carlbom <tc@kentima.se>
2021-09-12 15:07:49 +0200

    rename of project

--- widequick-designer-project/RemoteClient_v1_0_1/Scripts/scWM_v1_0_1.js
+++
@@ -1,6 +0,0 @@
-windowManager.onWindowCreated = function(ev){
-	if(this.firstView == undefined){
-		this.firstView = ev.view
-		windowManager.moveTo(windowManager.firstView, windowManager.Center)
-	}
-}
